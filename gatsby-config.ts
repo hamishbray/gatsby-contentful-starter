66d057651a46ade14120a846bb3c740c292c0166
Hamish Bray <hamish.bray@empired.com>
2021-04-07 09:18:32 +1000

    algolia indexing

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -12,6 +12,51 @@ export const flags = {
 	FAST_DEV: true,
 }
 
+const queries = [
+	{
+		query: `
+			{
+				allContentfulBlogPost {
+					nodes {
+						body {
+							childMarkdownRemark {
+								html
+							}
+						}
+						description {
+							childMarkdownRemark {
+								html
+							}
+						}
+						heroImage {
+							file {
+								url
+							}
+							gatsbyImageData(width: 250)
+						}
+						objectID: id
+						published: publishDate
+						tags
+						title
+						modified: updatedAt
+					}
+				}
+			}
+		`,
+		settings: {
+			searchableAttributes: ['title', 'content', 'summary', 'tags'],
+			attributesForFaceting: ['tags'],
+		},
+		transformer: ({ data }: { data: any }) =>
+			data.allContentfulBlogPost.nodes.map((node: any) => {
+				node.content = node.body?.childMarkdownRemark.html
+				node.summary = node.description?.childMarkdownRemark.html
+				node.image = node.heroImage?.file.url
+				return node
+			}),
+	},
+]
+
 export const plugins = [
 	'gatsby-plugin-postcss',
 	'gatsby-plugin-typescript',
@@ -46,6 +91,17 @@ export const plugins = [
 			accessToken:
 				process.env.CONTENTFUL_ACCESS_TOKEN ||
 				process.env.CONTENTFUL_DELIVERY_TOKEN,
+		},
+	},
+	{
+		resolve: `gatsby-plugin-algolia`,
+		options: {
+			appId: process.env.GATSBY_ALGOLIA_APP_ID,
+			apiKey: process.env.ALGOLIA_API_KEY,
+			indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
+			queries,
+			enablePartialUpdates: true,
+			matchFields: ['modified'],
 		},
 	},
 	`gatsby-plugin-offline`,
--- src/node-utils/onCreateNode.ts
+++ src/node-utils/onCreateNode.ts
@@ -1,28 +1,19 @@
 // @ts-nocheck - lots of unknown types below. e.g. node.elements
 import { GatsbyNode } from 'gatsby'
-import slugify from '@sindresorhus/slugify'
 import crypto from 'crypto'
-import { createRemoteFileNode } from 'gatsby-source-filesystem'
 
 export const onCreateNode: GatsbyNode['onCreateNode'] = ({
 	node,
-	actions: { createNodeField, createNode, createParentChildLink },
+	actions: { createNode, createParentChildLink },
 	createNodeId,
-	store,
-	cache,
 }) => {
-	const createField = (name: string, value: string): void =>
-		createNodeField({
-			node,
-			name,
-			value,
-		})
-
 	const createSearchableItemNode = (typeFieldData: Object): void => {
 		const fieldData = {
-			modified: node.system.last_modified,
-			modified_unix: toUnix(node.system.last_modified),
-			type: node.system.type,
+			modified: node.updatedAt,
+			modified_unix: toUnix(node.updatedAt),
+			published: node.publishDate,
+			published_unix: toUnix(node.publishDate),
+			type: node.internal.type,
 			...typeFieldData,
 		}
 
@@ -44,62 +35,20 @@ export const onCreateNode: GatsbyNode['onCreateNode'] = ({
 		createParentChildLink({ parent: node, child: nodeData })
 	}
 
-	const createRemoteImageFileNode = async (): void => {
-		let fileNode
-
-		try {
-			fileNode = await createRemoteFileNode({
-				url: node.elements.image.value[0].url,
-				parentNodeId: node.id,
-				createNode,
-				createNodeId,
-				cache,
-				store,
-			})
-		} catch (error) {
-			console.log('Error creating remote file node: ', error)
-		}
-
-		if (fileNode) node.featuredImg___NODE = fileNode.id
-	}
-
 	let typeFieldData = {}
 
 	switch (node.internal.type) {
-		case 'kontent_item_accessory':
-			createField('slug', node.elements.url_pattern.value)
+		case 'ContentfulBlogPost':
 			typeFieldData = {
-				content: node.elements.long_description.value,
-				summary: node.elements.short_description.value,
-				title: node.elements.product_name.value,
-				imageUrl: node.elements.image.value[0].url,
-				url: `/${node.system.type}/${node.fields.slug}`,
+				content: node.body?.childMarkdownRemark.html ?? 'Content',
+				summary: node.description?.childMarkdownRemark.html ?? 'Summary',
+				title: node.title,
+				imageUrl: node.heroImage?.file.url,
+				tags: node.tags,
+				url: `/blog/${node.slug}`,
 			}
 			createSearchableItemNode(typeFieldData)
 			break
-
-		case 'kontent_item_article':
-			createField('slug', node.elements.url_pattern.value)
-			typeFieldData = {
-				content: node.elements.body_copy.value,
-				summary: node.elements.summary.value,
-				title: node.elements.title.value,
-				published: node.elements.post_date.value,
-				published_unix: toUnix(node.elements.post_date.value),
-				imageUrl: node.elements.teaser_image.value[0].url,
-				url: `/${node.system.type}/${node.fields.slug}`,
-			}
-			createSearchableItemNode(typeFieldData)
-			break
-
-		case 'kontent_item_cafe':
-			createField('slug', slugify(node.elements.city.value))
-			typeFieldData = {
-				title: node.elements.city.value,
-				imageUrl: node.elements.photo.value[0].url,
-				url: `/${node.system.type}/${node.fields.slug}`,
-			}
-			createSearchableItemNode(typeFieldData)
 	}
 }
 
