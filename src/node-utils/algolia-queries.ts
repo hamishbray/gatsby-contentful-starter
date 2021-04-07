07ceb7e15f5c4a25d5a997c9ec0742ebe33c4d2b
Hamish Bray <hamish.bray@empired.com>
2021-04-07 15:41:04 +1000

    refactor algolia queries

---
+++ src/node-utils/algolia-queries.ts
@@ -0,0 +1,85 @@
+export const queries = [
+	{
+		query: `
+			{
+				allContentfulBlogPost {
+					nodes {
+						body {
+							childMarkdownRemark {
+								html
+							}
+						}
+						description {
+							childMarkdownRemark {
+								html
+							}
+						}
+						heroImage {
+							file {
+								url
+							}
+							gatsbyImageData(width: 225, height: 150)
+						}
+						objectID: id
+						published: publishDate
+						slug
+						tags
+						title
+						modified: updatedAt
+					}
+				}
+			}
+		`,
+		settings: {
+			searchableAttributes: ['title', 'content', 'summary', 'tags'],
+			attributesForFaceting: ['tags', 'type'],
+		},
+		transformer: ({ data }: { data: any }) =>
+			data.allContentfulBlogPost.nodes.map((node: any) => {
+				node.content = node.body?.childMarkdownRemark.html
+				node.summary = node.description?.childMarkdownRemark.html
+				node.image = node.heroImage?.file.url
+				node.url = `/blog/${node.slug}`
+				node.type = 'Blog Post'
+				return node
+			}),
+	},
+	{
+		query: `
+			{
+				allContentfulPerson {
+					nodes {
+						published: createdAt
+						modified: updatedAt
+						title: name
+						shortBio {
+							childMarkdownRemark {
+								html
+							}
+						}
+						objectID: id
+						heroImage: image {
+							file {
+								url
+							}
+							gatsbyImageData(width: 225, height: 225)
+						}
+						slug
+					}
+				}
+			}
+		`,
+		settings: {
+			searchableAttributes: ['title', 'summary'],
+			attributesForFaceting: ['type'],
+		},
+		transformer: ({ data }: { data: any }) =>
+			data.allContentfulPerson.nodes.map((node: any) => {
+				node.summary = node.shortBio?.childMarkdownRemark.html
+				node.image = node.heroImage?.file.url
+				node.url = `/bio/${node.slug}`
+				node.type = 'Person'
+				return node
+			}),
+	},
+]
