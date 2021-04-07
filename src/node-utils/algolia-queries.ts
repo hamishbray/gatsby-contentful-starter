ea6e856d9e4af136f8a2a31f874122e38faac123
Hamish Bray <hamish.bray@empired.com>
2021-04-07 15:44:51 +1000

    further refinement of algolia queries

--- src/node-utils/algolia-queries.ts
+++ src/node-utils/algolia-queries.ts
@@ -1,85 +1,86 @@
-export const queries = [
-	{
-		query: `
-			{
-				allContentfulBlogPost {
-					nodes {
-						body {
-							childMarkdownRemark {
-								html
-							}
+const blogQuery = {
+	query: `
+		{
+			allContentfulBlogPost {
+				nodes {
+					body {
+						childMarkdownRemark {
+							html
 						}
-						description {
-							childMarkdownRemark {
-								html
-							}
-						}
-						heroImage {
-							file {
-								url
-							}
-							gatsbyImageData(width: 225, height: 150)
-						}
-						objectID: id
-						published: publishDate
-						slug
-						tags
-						title
-						modified: updatedAt
 					}
+					description {
+						childMarkdownRemark {
+							html
+						}
+					}
+					heroImage {
+						file {
+							url
+						}
+						gatsbyImageData(width: 225, height: 150)
+					}
+					objectID: id
+					published: publishDate
+					slug
+					tags
+					title
+					modified: updatedAt
 				}
 			}
-		`,
-		settings: {
-			searchableAttributes: ['title', 'content', 'summary', 'tags'],
-			attributesForFaceting: ['tags', 'type'],
-		},
-		transformer: ({ data }: { data: any }) =>
-			data.allContentfulBlogPost.nodes.map((node: any) => {
-				node.content = node.body?.childMarkdownRemark.html
-				node.summary = node.description?.childMarkdownRemark.html
-				node.image = node.heroImage?.file.url
-				node.url = `/blog/${node.slug}`
-				node.type = 'Blog Post'
-				return node
-			}),
+		}
+	`,
+	settings: {
+		searchableAttributes: ['title', 'content', 'summary', 'tags'],
+		attributesForFaceting: ['tags', 'type'],
 	},
-	{
-		query: `
-			{
-				allContentfulPerson {
-					nodes {
-						published: createdAt
-						modified: updatedAt
-						title: name
-						shortBio {
-							childMarkdownRemark {
-								html
-							}
+	transformer: ({ data }: { data: any }) =>
+		data.allContentfulBlogPost.nodes.map((node: any) => {
+			node.content = node.body?.childMarkdownRemark.html
+			node.summary = node.description?.childMarkdownRemark.html
+			node.image = node.heroImage?.file.url
+			node.url = `/blog/${node.slug}`
+			node.type = 'Blog Post'
+			return node
+		}),
+}
+
+const personQuery = {
+	query: `
+		{
+			allContentfulPerson {
+				nodes {
+					published: createdAt
+					modified: updatedAt
+					title: name
+					shortBio {
+						childMarkdownRemark {
+							html
 						}
-						objectID: id
-						heroImage: image {
-							file {
-								url
-							}
-							gatsbyImageData(width: 225, height: 225)
-						}
-						slug
 					}
+					objectID: id
+					heroImage: image {
+						file {
+							url
+						}
+						gatsbyImageData(width: 225, height: 225)
+					}
+					slug
 				}
 			}
-		`,
-		settings: {
-			searchableAttributes: ['title', 'summary'],
-			attributesForFaceting: ['type'],
-		},
-		transformer: ({ data }: { data: any }) =>
-			data.allContentfulPerson.nodes.map((node: any) => {
-				node.summary = node.shortBio?.childMarkdownRemark.html
-				node.image = node.heroImage?.file.url
-				node.url = `/bio/${node.slug}`
-				node.type = 'Person'
-				return node
-			}),
+		}
+	`,
+	settings: {
+		searchableAttributes: ['title', 'summary'],
+		attributesForFaceting: ['type'],
 	},
-]
+	transformer: ({ data }: { data: any }) =>
+		data.allContentfulPerson.nodes.map((node: any) => {
+			node.summary = node.shortBio?.childMarkdownRemark.html
+			node.image = node.heroImage?.file.url
+			node.url = `/person/${node.slug}`
+			node.type = 'Person'
+			return node
+		}),
+}
+
+export const queries = [blogQuery, personQuery]
