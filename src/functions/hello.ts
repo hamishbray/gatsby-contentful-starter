86bb2e8e718eddfafa269c03998bc4e38ec834d9
Hamish Bray <hamish.bray@empired.com>
2021-04-20 17:12:04 +1000

    context in hello lambda function

--- src/functions/hello.ts
+++ src/functions/hello.ts
@@ -8,9 +8,14 @@ import {
 export const handler: Handler = async (
 	event: HandlerEvent,
 	context: HandlerContext
-): Promise<HandlerResponse> => ({
-	statusCode: 200,
-	body: JSON.stringify({
-		message: `Hello world ${Math.floor(Math.random() * 10)}`,
-	}),
-})
+): Promise<HandlerResponse> => {
+	console.log(`hello event:`, JSON.stringify(event))
+	console.log(`hello context:`, JSON.stringify(context))
+
+	return {
+		statusCode: 200,
+		body: JSON.stringify({
+			message: `Hello world ${Math.floor(Math.random() * 10)}`,
+		}),
+	}
+}
