365ee3a5f73439b48e1aa500bb479a3bd04a6258
Hamish Bray <hamish.bray@empired.com>
2021-04-20 14:35:18 +1000

    clean up

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,78 +2,80 @@ require('dotenv').config({
 	path: `.env.${process.env.NODE_ENV}`,
 })
 
+import { GatsbyConfig } from 'gatsby'
 import { createProxyMiddleware } from 'http-proxy-middleware'
 
 import { queries } from './src/node-utils/algolia-queries'
 
-export const developMiddleware = (app: any) => {
-	app.use(
-		'/.netlify/functions/',
-		createProxyMiddleware({
-			target: 'http://localhost:9000',
-			pathRewrite: {
-				'/.netlify/functions/': '',
+const config: GatsbyConfig = {
+	developMiddleware: (app: any) => {
+		app.use(
+			'/.netlify/functions/',
+			createProxyMiddleware({
+				target: 'http://localhost:9000',
+				pathRewrite: {
+					'/.netlify/functions/': '',
+				},
+			})
+		)
+	},
+	siteMetadata: {
+		title: 'Gatsby Contentful POC',
+		description: 'Gatsby starter site with Contentful source',
+		author: '@hamishbray',
+	},
+	flags: {
+		FAST_DEV: true,
+	},
+	plugins: [
+		'gatsby-plugin-postcss',
+		'gatsby-plugin-typescript',
+		'gatsby-plugin-react-helmet',
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'images',
+				path: `${__dirname}/src/images`,
 			},
-		})
-	)
+		},
+		'gatsby-plugin-image',
+		`gatsby-transformer-remark`,
+		'gatsby-transformer-sharp',
+		'gatsby-plugin-sharp',
+		{
+			resolve: 'gatsby-plugin-manifest',
+			options: {
+				name: 'gatsby-starter-default',
+				short_name: 'starter',
+				start_url: '/',
+				background_color: '#663399',
+				theme_color: '#663399',
+				display: 'standalone',
+				icon: 'src/images/heat.png', // This path is relative to the root of the site.
+			},
+		},
+		{
+			resolve: `gatsby-source-contentful`,
+			options: {
+				spaceId: process.env.CONTENTFUL_SPACE_ID,
+				accessToken:
+					process.env.CONTENTFUL_ACCESS_TOKEN ||
+					process.env.CONTENTFUL_DELIVERY_TOKEN,
+			},
+		},
+		{
+			resolve: `gatsby-plugin-algolia`,
+			options: {
+				appId: process.env.GATSBY_ALGOLIA_APP_ID,
+				apiKey: process.env.ALGOLIA_API_KEY,
+				indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
+				queries,
+				enablePartialUpdates: true,
+				matchFields: ['modified'],
+			},
+		},
+		`gatsby-plugin-offline`,
+	],
 }
 
-export const siteMetadata = {
-	title: 'Gatsby Contentful POC',
-	description: 'Gatsby starter site with Contentful source',
-	author: '@hamishbray',
-}
-
-export const flags = {
-	FAST_DEV: true,
-}
-
-export const plugins = [
-	'gatsby-plugin-postcss',
-	'gatsby-plugin-typescript',
-	'gatsby-plugin-react-helmet',
-	{
-		resolve: 'gatsby-source-filesystem',
-		options: {
-			name: 'images',
-			path: `${__dirname}/src/images`,
-		},
-	},
-	'gatsby-plugin-image',
-	`gatsby-transformer-remark`,
-	'gatsby-transformer-sharp',
-	'gatsby-plugin-sharp',
-	{
-		resolve: 'gatsby-plugin-manifest',
-		options: {
-			name: 'gatsby-starter-default',
-			short_name: 'starter',
-			start_url: '/',
-			background_color: '#663399',
-			theme_color: '#663399',
-			display: 'standalone',
-			icon: 'src/images/heat.png', // This path is relative to the root of the site.
-		},
-	},
-	{
-		resolve: `gatsby-source-contentful`,
-		options: {
-			spaceId: process.env.CONTENTFUL_SPACE_ID,
-			accessToken:
-				process.env.CONTENTFUL_ACCESS_TOKEN ||
-				process.env.CONTENTFUL_DELIVERY_TOKEN,
-		},
-	},
-	{
-		resolve: `gatsby-plugin-algolia`,
-		options: {
-			appId: process.env.GATSBY_ALGOLIA_APP_ID,
-			apiKey: process.env.ALGOLIA_API_KEY,
-			indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
-			queries,
-			enablePartialUpdates: true,
-			matchFields: ['modified'],
-		},
-	},
-	`gatsby-plugin-offline`,
-]
+module.exports = config
--- src/functions/hello.ts
+++ src/functions/hello.ts
@@ -1,6 +1,14 @@
-import { Context, APIGatewayEvent } from 'aws-lambda'
+import {
+	Handler,
+	HandlerContext,
+	HandlerEvent,
+	HandlerResponse,
+} from '@netlify/functions'
 
-export const handler = async (event: APIGatewayEvent, context: Context) => ({
+export const handler: Handler = async (
+	event: HandlerEvent,
+	context: HandlerContext
+): Promise<HandlerResponse> => ({
 	statusCode: 200,
 	body: JSON.stringify({
 		message: `Hello world ${Math.floor(Math.random() * 10)}`,
