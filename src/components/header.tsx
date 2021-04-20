bf18e03e071c97f99d88e3c2614f787b3d5fa868
Hamish Bray <hamish.bray@empired.com>
2021-04-20 16:35:12 +1000

    added identity

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,26 +1,40 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { useIdentityContext } from 'react-netlify-identity-widget'
 
 import logo from '../images/contentful-logo.svg'
 
 type Props = {
 	siteTitle: string
+	setDialog: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-const Header = ({ siteTitle }: Props) => (
-	<header className="mb-6 bg-yellow-900">
-		<div className="flex max-w-screen-lg px-4 py-8 mx-auto">
-			<div className="inline-flex mr-3">
-				<img className="h-10" src={logo} alt="Contentful Logo" />
+const Header = ({ siteTitle, setDialog }: Props) => {
+	const { user, isLoggedIn } = useIdentityContext()
+	const name = user?.user_metadata?.full_name ?? 'Mr Nobody'
+
+	return (
+		<header className="mb-6 bg-yellow-900">
+			<div className="flex max-w-screen-lg px-4 py-8 mx-auto">
+				<div className="inline-flex mr-3">
+					<img className="h-10" src={logo} alt="Contentful Logo" />
+				</div>
+				<div className="inline-flex text-4xl">
+					<Link to="/" className="text-white no-underline">
+						{siteTitle}
+					</Link>
+				</div>
+				<nav className="inline pl-8 text-white">
+					<button
+						className="inline px-4 py-2 border-2 border-white rounded hover:border-black hover:text-black"
+						onClick={() => setDialog(true)}
+					>
+						{isLoggedIn ? `Hello ${name}, Log out here!` : 'LOG IN'}
+					</button>
+				</nav>
 			</div>
-			<div className="inline-flex text-4xl">
-				<Link to="/" className="text-white no-underline">
-					{siteTitle}
-				</Link>
-			</div>
-			<nav className="inline pl-8 text-white"></nav>
-		</div>
-	</header>
-)
+		</header>
+	)
+}
 
 export default Header
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
+import IdentityModal from 'react-netlify-identity-widget'
 
 import '@reach/tabs/styles.css'
+import 'react-netlify-identity-widget/styles.css'
 
 import Header from './header'
 
@@ -10,6 +12,8 @@ interface Props {
 }
 
 const Layout = ({ children }: Props) => {
+	const [dialog, setDialog] = useState(false)
+
 	const data = useStaticQuery(graphql`
 		query SiteTitleQuery {
 			site {
@@ -22,11 +26,19 @@ const Layout = ({ children }: Props) => {
 
 	return (
 		<>
-			<Header siteTitle={data.site.siteMetadata?.title || ``} />
+			<Header
+				siteTitle={data.site.siteMetadata?.title || ``}
+				setDialog={setDialog}
+			/>
 			<div className="box-border max-w-screen-lg px-4 py-8 mx-auto">
 				<main>{children}</main>
 				<footer className="mt-8">© {new Date().getFullYear()}</footer>
 			</div>
+			<IdentityModal
+				aria-label="login modal"
+				showDialog={dialog}
+				onCloseDialog={() => setDialog(false)}
+			/>
 		</>
 	)
 }
