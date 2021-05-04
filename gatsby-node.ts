8c2c3ab31b07e0785e72c5e9725306ef2ba28d63
Hamish Bray <hamish.bray@empired.com>
2021-05-04 15:48:23 +1000

    removed gatsby config

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,4 +1,5 @@
-export { createPages } from './src/node-utils/createPages'
-export { onCreatePage } from './src/node-utils/onCreatePage'
+//export { createPages } from './src/node-utils/createPages'
+//export { onCreatePage } from './src/node-utils/onCreatePage'
 //export { createSchemaCustomization } from './src/node-utils/createSchemaCustomization'
 //export { onCreateNode } from './src/node-utils/onCreateNode'
+export {}
